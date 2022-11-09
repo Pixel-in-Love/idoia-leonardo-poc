f9695df8a6406ec2f1382108e61bc21db742e634
Rubén Villar Grela <ruben@rubenvillar.net>
2022-11-09 08:37:23 +0100

    change backgroundsize

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const img = document.querySelector(".image");
+const img = document.querySelector(".hero-img");
 const menuItems = document.querySelectorAll(".menu-item");
+const coverButton = document.querySelector(".cover");
+const containButton = document.querySelector(".contain");
 
 menuItems.forEach((item) => {
   item.addEventListener("click", () => {
     const content = item.textContent
     if(content === "Inicio") {
-      img.src = "bg2x.png"
+      img.style.backgroundImage = "url(bg2x.png)"
     }
     if(content === "Clases de tú a tú") {
-      img.src = "collage2x.png"
+      img.style.backgroundImage = "url(collage2x.png)"
     }
     if(content === "Study zone") {
-      img.src = "zone2x.png"
+      img.style.backgroundImage = "url(zone2x.png)"
     }
   })
+})
+
+coverButton.addEventListener("click", () => {
+  img.style.backgroundSize = "cover"
+})
+
+containButton.addEventListener("click", () => {
+  img.style.backgroundSize = "contain"
 })
